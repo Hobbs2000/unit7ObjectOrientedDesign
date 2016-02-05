73a6f00a9ef82b627a485da3de569f27017cf0a9
Hobbs2000 <cjfield@NN123-HA-07.HS.sd203.org>
2016-02-05 14:08:23 -0600

    Worked on practice

--- Unit 7 practice prompt/Test.java
+++ Unit 7 practice prompt/Test.java
@@ -2,18 +2,15 @@ public class Test
 {
     public static void main(String args[])
     {
-        int num1 = 160;
-        System.out.println("Num1: "+Integer.toBinaryString(num1));
-        int num2 = 240;
-        System.out.println("Num2: "+Integer.toBinaryString(num2));
-        num2 = num2 >> 4;
-        System.out.println("Shifted Num2: "+Integer.toBinaryString(num2));
-        int newNum = num1 | num2;
-        System.out.println("New combined num: "+Integer.toBinaryString(newNum));
+        int R = 24;
+        int G = 1;
+        int B = 100;
+        int RGB = (R)|(G<<8)|(B<<16);
+        System.out.println("RGB vals as one 16-bit int in binary: "+Integer.toBinaryString(RGB));
         
-        int reShiftedNum2 = (newNum & (newNum >> 4));
-        System.out.println("Combined num shifted back to num2: "+Integer.toBinaryString(reShiftedNum2));
-        int reShiftedNum1 = (newNum & (newNum << 4)); 
-        System.out.println("Combined num shifted back to num1: "+Integer.toBinaryString(reShiftedNum1));
+        int newR =  RGB & 0xFF;
+        int newG = (RGB & 0xFF00) >> 8;
+        int newB =  RGB >> 16;
+        System.out.println("Unpacked 16-bit RGB\nR: "+newR+"\nG: "+newG+"\nB: "+newB);
     }
 }
