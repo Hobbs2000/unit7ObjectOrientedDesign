2010f9143cc6e1ff62d89e6bf89595f42cbe346f
Hobbs2000 <cjfield@NN123-HA-07.HS.sd203.org>
2016-02-10 14:24:41 -0600

    Worked on practice with GUIs

--- Unit 7 practice prompt/ButtonViewer.java
+++ Unit 7 practice prompt/ButtonViewer.java
@@ -1,23 +1,72 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+/**
+ * 
+ */
 public class ButtonViewer
 {
     private static final int FRAME_WIDTH = 100;
     private static final int FRAME_HEIGHT = 60;
     
-    public static void main(String[] args)
+    private JFrame frame;
+    private JPanel panel;
+    private JButton button;
+    private JButton button2;
+    private JLabel label;
+    private ClickListener listener;
+    
+    /**
+     * 
+     */
+    public ButtonViewer()
     {
-        JFrame frame = new JFrame();
-        JButton b1 = new JButton("Click Me!");
-        frame.add(b1);
+        this.frame = new JFrame();
         
-        ClickListener listener = new ClickListener();
-        b1.addActionListener(listener);
+        panel = new JPanel();
+        frame.add(panel);
+        
+        label = new JLabel();
+        panel.add(label);
+        
+        button = new JButton("Click Me!");
+        panel.add(button);
+        
+        button2 = new JButton("Click Me Too!");
+        panel.add(button2);
+        
+        listener = new ClickListener();
+        button.addActionListener(listener);
+        button2.addActionListener(listener);
         
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+    
+    /**
+     * 
+     */
+    public static void main(String[] args)
+    {
+        ButtonViewer bViewer = new ButtonViewer();
+    }
+    
+    
+    
+    /**
+    * An action listener that prints a message 
+    */
+    public class ClickListener implements ActionListener
+    {    
+        public void actionPerformed(ActionEvent event)
+        {
+            label.setText(event.getActionCommand()+" was clicked.");
+        }
     }
 }
